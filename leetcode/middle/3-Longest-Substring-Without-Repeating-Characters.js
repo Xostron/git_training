a19c7986f83c4842ff2fe0caf839a0c59e993048
Xostron <aat9044107480@gmail.com>
2025-03-22 22:46:20 +0300

    fix 3

--- leetcode/middle/3-Longest-Substring-Without-Repeating-Characters.js
+++ leetcode/middle/3-Longest-Substring-Without-Repeating-Characters.js
@@ -23,6 +23,31 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    
-};
+var lengthOfLongestSubstring = function (s) {
+	let result = ''
+	let cur = ''
+	s.split('').forEach((el, i) => {
+		// Уникальный набор
+		if (!cur.includes(el)) {
+			cur += el
+			// еще есть символы - продолжаем собирать набор
+			if (i < s.length - 1) return
+		}
+		// Последний символ || символ повторился в наборе - сброс и сохранение
+		// Набор больше предыдущего
+		if (cur.length > result.length) result = cur
+		// Набор меньше предыдущего
+		cur = el
+	})
+    console.log(s, result, result.length)
+    return result.length
+}
+
+
+const s = "abcabcbb"
+const s1 = "bbbbb"
+const s2 = "pwwkew"
+
+lengthOfLongestSubstring(s)
+lengthOfLongestSubstring(s1)
+lengthOfLongestSubstring(s2)
