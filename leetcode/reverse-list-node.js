7dde8123d44a121cf58636b8558115d1a31bca61
Xostron <aat9044107480@gmail.com>
2025-03-24 23:26:27 +0300

    fix, docs

--- leetcode/reverse-list-node.js
+++ leetcode/reverse-list-node.js
@@ -8,16 +8,34 @@ const list = { val: 1, next: { val: 2, next: { val: 3, next: { val: 4, next: { v
 // result = { val: 5, next: { val: 4, next: { val: 3, next: { val: 2, next: { val: 1, next: null } } } } }
 
 let prev, cur, next
-function reverse(obj, next, prev, idCur = 0) {
-	if (next === null) return
+/**
+ * 
+ * @param {*} obj Связный список
+ * @param {*} next следующий/текущий элемент
+ * @param {*} prev Предыдущий эемент
+ * @param {*} idCur Текщая глуюина
+ * @param {*} limit ограничитель глубины погружения в связный список 
+ * @returns 
+ */
+function reverse(obj, next, prev, idCur = 0, limit) {
+	// Конец обработки
+	if (limit === 1) {
+		console.log('FINISH', idCur, limit)
+		return
+	}
+    // Конец итерации: конец связного списка || конец обработки на данной глубине
+	if (next === null || idCur === limit) return reverse(list, obj.next, list, 0, idCur - 1)
+    // Второй элемент - здесь можно менять местами предыдущий-текущий
 	if (idCur > 0) {
-		console.log(111, idCur, next, prev)
+		// console.log(111, idCur, next, prev)
 		const temp = prev.val
 		prev.val = next.val
 		next.val = temp
 	}
-	if (idCur === 0) return reverse(list, obj.next, list, ++idCur)
-	return reverse(list, next.next, next, ++idCur)
+    // Первый элемент: даем эстафету
+	if (idCur === 0) return reverse(list, obj.next, list, ++idCur, limit)
+    // Последующие элементы: сдесь происходит перестановка
+	return reverse(list, next.next, next, ++idCur, limit)
 }
 
 reverse(list)
