e399d6b3f8fb808d9455066ba74f25ce0174e1f2
Xostron <aat9044107480@gmail.com>
2025-03-27 10:05:08 +0300

    fix

--- our_test/task03.js
+++ our_test/task03.js
@@ -4,13 +4,41 @@
  * Задача - найти все вхождения подстроки в строке "как слово".
  *
  */
-const s = 'Молоковоз везет молоко, и на нем большими буквами написано МОЛОКО'
+const s = 'Молоковоз везет  #молоко, и на нем большими буквами написано !МОЛОКО.'
 const ss = 'молоко'
 
 function find(s, ss) {
-    
-	return []
+	const r = []
+	s.split(' ').forEach((el) => {
+		const str = el.replace(/[,.!#]/g, '')
+		if (str.toLowerCase() === ss.toLowerCase()) r.push(str)
+	})
+	return r
 }
 
 const r = find(s, ss)
 console.log(r)
+
+
+
+const str = 'Какая-то строка с очень интересным ааоченьоо оченьаа ааочень аоченьа очень. содержанием и очень чем-то еще интересным';
+const entry = 'очень';
+function matches() {
+  const allowed = new Set([undefined,'',' ', '.', '!', '"', '\'', ',', '?']);
+  return (str, entry) => {
+    const arr = [];
+    let i = str.indexOf(entry);
+    while (i !== -1) {
+      const left = str[i-1];
+      const right = str[i + entry.length];
+      if ([left, right].every(v => allowed.has(v))) arr.push(i)
+      i = str.indexOf(entry, ++i);
+    }
+    return arr;
+  }
+}
+
+// const result = lgMatches(str, entry);
+const fn = matches();
+const result = fn(str, entry);
+console.log(result);
--- our_test/task04.js
+++ our_test/task04.js
@@ -1,80 +1,99 @@
 /**
  * @description Задание №4
  * Из начального массива array получить нужный массив result
- * 
+ *
  */
-const result = [
-   {'2024_3': ['Абакан', 'Воронеж']},
-   {'2024_4': ['Омск', 'Воронеж']},
-]
+const result = [{ '2024_3': ['Абакан', 'Воронеж'] }, { '2024_4': ['Омск', 'Воронеж'] }]
 const array = [
-  {
-    "2024_3": [
-      {
-        "Абакан": [
-          {
-            "Петрушник С.": [
-              {
-                "director": "Петрушник С.",
-                "completion_date": "12.09.2024",
-                "total_task": 3
-              }
-            ]
-          },
-          {
-            "Сидоров И.": [
-              {
-                "director": "Сидоров И.",
-                "completion_date": "14.09.2024",
-                "total_task": 2
-              }
-            ]
-          }
-        ]
-      },
-      {
-        "Воронеж": [
-          {
-            "Сидоров И.": [
-              {
-                "director": "Сидоров И.",
-                "completion_date": "16.09.2024",
-                "total_task": 1
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  },
-  {
-    "2024_4": [
-      {
-        "Омск": [
-          {
-            "Хрюкин А.": [
-              {
-                "director": "Хрюкин А.",
-                "completion_date": "13.11.2024",
-                "total_task": 4
-              }
-            ]
-          }
-        ]
-      },
-      {
-        "Воронеж": [
-          {
-            "Сидоров И.": [
-              {
-                "director": "Сидоров И.",
-                "completion_date": "15.11.2024",
-                "total_task": 2
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
+	{
+		'2024_3': [
+			{
+				Абакан: [
+					{
+						'Петрушник С.': [
+							{
+								director: 'Петрушник С.',
+								completion_date: '12.09.2024',
+								total_task: 3,
+							},
+						],
+					},
+					{
+						'Сидоров И.': [
+							{
+								director: 'Сидоров И.',
+								completion_date: '14.09.2024',
+								total_task: 2,
+							},
+						],
+					},
+				],
+			},
+			{
+				Воронеж: [
+					{
+						'Сидоров И.': [
+							{
+								director: 'Сидоров И.',
+								completion_date: '16.09.2024',
+								total_task: 1,
+							},
+						],
+					},
+				],
+			},
+		],
+	},
+	{
+		'2024_4': [
+			{
+				Омск: [
+					{
+						'Хрюкин А.': [
+							{
+								director: 'Хрюкин А.',
+								completion_date: '13.11.2024',
+								total_task: 4,
+							},
+						],
+					},
+				],
+			},
+			{
+				Воронеж: [
+					{
+						'Сидоров И.': [
+							{
+								director: 'Сидоров И.',
+								completion_date: '15.11.2024',
+								total_task: 2,
+							},
+						],
+					},
+				],
+			},
+		],
+	},
 ]
+
+function transform(array) {
+	const r = []
+	array.forEach((obj) => {
+		const data = Object.entries(obj)
+		data.forEach(([key, a1]) => {
+			const rr = {}
+			rr[key] = []
+			a1.forEach((o) => {
+				rr[key].push(...Object.keys(o))
+			})
+			r.push(rr)
+		})
+	})
+	return r
+}
+
+const r = transform(array)
+console.log(333, r)
+
+
+const pack = data => data.flatMap(o => Object.entries(o).map(([year, d]) => ({ [year]: d.flatMap(v => Object.keys(v)) })));
