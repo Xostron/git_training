ae24bf9f5f269a1f66ad2e6644ee07717487bd8a
Xostron <aat9044107480@gmail.com>
2025-04-22 17:06:24 +0300

    fix

---
+++ leetcode/middle/1004. Max Consecutive Ones III.js
@@ -0,0 +1,34 @@
+/**
+ * Дан двоичный массив nums и целое число k.
+ * Верните максимальное количество последовательных единиц в массиве,
+ * если вы можете заменить не более k нулей на единицы.
+ *
+ *
+ * Example 1:
+ * Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+ * Output: 6
+ * Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+ * Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ *
+ * Example 2:
+ * Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+ * Output: 10
+ * Explanation:  [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+ * Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ *
+ *
+ * Constraints:
+ * 1 <= nums.length <= 105
+ * nums[i] is either 0 or 1.
+ * 0 <= k <= nums.length
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function (nums, k) {}
+
+console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2)) //6
+console.log(longestOnes([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3)) //10
