61ddcfe21fe0677b3ec07d313a1c2c7ba289c214
Xostron <aat9044107480@gmail.com>
2025-05-01 23:19:24 +0300

    649

---
+++ leetcode/easy/206. Reverse Linked List.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+function ListNode(val, next) {
+	this.val = val === undefined ? 0 : val
+	this.next = next === undefined ? null : next
+}
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {}
+
+const list = new ListNode(1,2)
+console.log(list)
