a7a215af23759bc67c5ff05d29824714505500ec
Xostron <aat9044107480@gmail.com>
2025-05-02 02:11:17 +0300

    2095

--- leetcode/middle/2095. Delete the Middle Node of a Linked List.js
+++ leetcode/middle/2095. Delete the Middle Node of a Linked List.js
@@ -16,22 +16,36 @@
 var deleteMiddle = function (head) {
 	let current = head,
 		size = 0,
-		stack = [],
-		prefix
+		stack = []
 	while (current !== null) {
-		if (size === 0) prefix = { val: current.val, next: {} }
-		else {
-			prefix.next.val = current.val
-			prefix.next.next = {}
-		}
-        console.log(size, prefix)
-		stack.push([prefix, current.next])
-
+		stack.push(current)
 		current = current.next
-		prefix = { ...prefix }
 		size++
 	}
-	console.log(JSON.stringify(stack, null, ' '))
+	const mid = Math.round(stack.length / 2)
+	stack[mid - 1].next = stack?.[mid + 1] ? stack?.[mid + 1] : null
+	return head
 }
 
 deleteMiddle({ val: 1, next: { val: 2, next: { val: 3, next: { val: 4, next: { val: 5, next: { val: 1, next: { val: 2, next: null } } } } } } })
+
+
+/**
+ * Алгоритм, быстрый и медленный указатель
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteMiddle = function(head) {
+    if (!head || !head.next) return null;
+  
+    let slow = head;
+    let fast = head;
+    let prev = null;
+    while (fast && fast.next) {
+      fast = fast.next.next;
+      prev = slow;
+      slow = slow.next;
+    }
+    prev.next = slow.next;
+    return head;
+  };
