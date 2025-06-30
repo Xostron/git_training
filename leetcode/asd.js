82327fda0720fe0b9ce89df767b378cc888cea57
Xostron <aat9044107480@gmail.com>
2025-07-01 00:10:49 +0300

    fix

--- leetcode/asd.js
+++ leetcode/asd.js
@@ -1,44 +1,34 @@
-function t() {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			console.log('timeout')
-			resolve(12)
-		}, 1000)
-	})
+function rr() {
+	return new Promise((resolve, reject) => resolve(42))
 }
 
-const proc = new Set()
-
-function rt(task) {
-	return new Promise((resolve, reject) => {
-		console.log('begin rt')
-		task()
-			.then((r) => {
-				console.log('resolve', r)
-				resolve(r)
-			})
-			.catch((e) => {
-				console.log('reject', e)
-				reject()
-			})
-			.finally((_) => {
-				console.log('finally rt')
-			})
-		console.log('end rt')
-	})
+async function rr1() {
+	return 12
 }
-async function qwe(){
-	console.log('qwe')
-}
-// rt(t)
-// 	.then((res) => {
-// 		console.log('result', res)
-// 	})
-// 	.finally((_) => console.log('finally'))
 
-console.log(1)
-await rt(t)
-console.log(2)
-await qwe()
-console.log(3)
-console.log(34)
+function rr2(){
+    return 1980
+}
+
+const t = rr()
+const t1 = rr1()
+const t2 = Promise.resolve(410)
+// t3,4,5 - делают лжно и тоже - распаковывае 
+const t3 = Promise.resolve(rr())
+const t4 = new Promise((resolve, reject) => {
+	rr().then(resolve)
+})
+const t5 = new Promise((resolve, reject) => {
+	resolve(rr())
+})
+const t6 = Promise.resolve(rr2())
+
+t.then((r) => console.log(0, r))
+t1.then((r) => console.log(1, r))
+t2.then((r) => console.log(2, r))
+t3.then((r) => console.log(3, r))
+t4.then((r) => console.log(4, r))
+t5.then(r=>console.log(5, r))
+t6.then(r=>console.log(6, r))
+const t7 = await rr2()
+console.log(7,t7)
